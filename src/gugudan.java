88354d83741c6464c4b6e74c63df1c9f85d032ae
seok2zzangzzangman <seok2zzangzzangman@localhost>
2022-03-16 14:16:35 +0900

    구구단 선행연

---
+++ src/gugudan.java
@@ -0,0 +1,17 @@
+
+public class gugudan {
+	public static void main(String[]args) {
+	int a=1;
+	int b=2;
+	int c=3;
+	int d=4;
+	float e=3;
+	System.out.println(a * b);
+	System.out.println(b * b);
+	System.out.println(b*c);
+	System.out.println(b*d);
+	System.out.println(b/e);
+	
+				
+	}
+}
